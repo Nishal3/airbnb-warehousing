90474144ba52c44a3b0a0e44ac9845bb1a690c24
Nishal Nepal <nepalnishal4@gmail.com>
2024-05-25 15:26:49 -0400

    Finalize table_creation file, and fix a naming error in listings_transorm JP notebook

--- mysql_connection_testing/table_creation.sql
+++ mysql_connection_testing/table_creation.sql
@@ -1,63 +1,63 @@
-CREATE TABLE `scrapings` (
-    `scrapings_id` int(11),
-    `scrape_id` int(11),
+CREATE TABLE IF NOT EXISTS `scrapings` (
+    `scrapings_id` int PRIMARY KEY,
+    `scrape_id` int,
     `last_scraped` date,
     `source` varchar(100)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `neighbourhoods` (
-    `neighbourhood_id` int(11),
+CREATE TABLE IF NOT EXISTS `neighbourhoods` (
+    `neighbourhood_id` int PRIMARY KEY,
     `neighbourhood` varchar(100),
     `neighbourhood_overview` varchar(1000),
     `neighbourhood_cleansed` varchar(100)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `minmax_insights` (
-    `minmax_insights_id` int(11),
-    `maximum_nights` int(11),
-    `minimum_nights` int(11),
-    `minimum_minimum_nights` int(11),
-    `maximum_minimum_nights` int(11),
-    `minimum_maximum_nights` int(11),
-    `maximum_maximum_nights` int(11),
-    `minimum_nights_avg_ntm` int(11),
-    `maximum_nights_avg_ntm` int(11),
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+CREATE TABLE IF NOT EXISTS `minmax_insights` (
+    `minmax_insights_id` int PRIMARY KEY,
+    `maximum_nights` int,
+    `minimum_nights` int,
+    `minimum_minimum_nights` int,
+    `maximum_minimum_nights` int,
+    `minimum_maximum_nights` int,
+    `maximum_maximum_nights` int,
+    `minimum_nights_avg_ntm` int,
+    `maximum_nights_avg_ntm` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `availability_info` (
-    `avail_id` int(11),
-    `has_availability` tinyint(1),
-    `availability_30` int(11),
-    `availability_60` int(11),
-    `availability_90` int(11),
-    `availability_365` int(11)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+CREATE TABLE IF NOT EXISTS `availability_info` (
+    `avail_id` int PRIMARY KEY,
+    `has_availability` tinyint,
+    `availability_30` int,
+    `availability_60` int,
+    `availability_90` int,
+    `availability_365` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `hld` (
-    `hld_id` int(11),
-    `calculated_host_listings_count` int(11),
-    `calculated_host_listings_count_entire_homes` int(11),
-    `calculated_host_listings_count_private_rooms` int(11),
-    `calculated_host_listings_count_shared_rooms` int(11)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+CREATE TABLE IF NOT EXISTS `hld` (
+    `hld_id` int PRIMARY KEY,
+    `calculated_host_listings_count` int,
+    `calculated_host_listings_count_entire_homes` int,
+    `calculated_host_listings_count_private_rooms` int,
+    `calculated_host_listings_count_shared_rooms` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `hqad` (
-    `hqad_id` int(11),
-    `response_time` varchar(100),
-    `response_rate` varchar(100),
-    `acceptance_rate` varchar(100),
-    `host_is_superhost` tinyint(1),
-    `listings_count` int(11),
-    `total_listings_count` int(11),
+CREATE TABLE IF NOT EXISTS `hqad` (
+    `hqad_id` int PRIMARY KEY,
+    `host_response_time` varchar(100),
+    `host_response_rate` varchar(100),
+    `host_acceptance_rate` varchar(100),
+    `host_is_superhost` tinyint,
+    `host_listings_count` int,
+    `host_total_listings_count` int,
     `host_verifications` varchar(100),
-    `host_has_profile_pic` tinyint(1),
-    `host_identity_verified` tinyint(1)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+    `host_has_profile_pic` tinyint,
+    `host_identity_verified` tinyint
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `host` (
-    `host_id` int(11),
-    `hld_id` int(11) FOREIGN KEY(hld_id) REFERENCES listing_diagnostics(listing_diagnostics_id),
-    `hqad_id` int(11) FOREIGN KEY(hqad_id) REFERENCES hqad(hqad_id),
+CREATE TABLE IF NOT EXISTS `host` (
+    `host_id` int PRIMARY KEY,
+    `hld_id` int,
+    `hqad_id` int,
     `host_url` varchar(100),
     `host_name` varchar(100),
     `host_since` date,
@@ -66,51 +66,68 @@ CREATE TABLE `host` (
     `host_thumbnail_url` varchar(100),
     `host_picture_url` varchar(100),
     `host_neighbourhood` varchar(100),
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+    FOREIGN KEY(hld_id) 
+        REFERENCES hld(hld_id),
+    FOREIGN KEY(hqad_id)
+        REFERENCES hqad(hqad_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `property` (
-    `property_id` int(11),
+CREATE TABLE IF NOT EXISTS `property` (
+    `property_id` int PRIMARY KEY,
     `latitude` varchar(100),
     `longitude` varchar(100),
     `property_type` varchar(100),
     `room_type` varchar(100),
-    `accommodates` int(11),
-    `bathrooms_text` int(11),
-    `bedrooms` int(11),
-    `beds` int(11),
+    `accommodates` int,
+    `bathrooms_text` int,
+    `beds` int,
     `amenities` varchar(1000),
     `price` varchar(100)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `reviews_diagnostics` (
-    `reviews_diagnostics_id` int(11),
-    `number_of_reviews` int(11),
-    `number_of_reviews_ltm` int(11),
+CREATE TABLE IF NOT EXISTS `reviews_diagnostics` (
+    `rev_diag_id` int PRIMARY KEY,
+    `number_of_reviews` int,
+    `number_of_reviews_ltm` int,
+    `number_of_reviews_l30d` int,
     `first_review` date,
     `last_review` date,
-    `review_scores_rating` int(11),
-    `review_scores_accuracy` int(11),
-    `review_scores_cleanliness` int(11),
-    `review_scores_checkin` int(11),
-    `review_scores_communication` int(11),
-    `review_scores_location` int(11),
-    `review_scores_value` int(11),
-    `reviews_per_month` int(11)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+    `review_scores_rating` int,
+    `review_scores_accuracy` int,
+    `review_scores_cleanliness` int,
+    `review_scores_checkin` int,
+    `review_scores_communication` int,
+    `review_scores_location` int,
+    `review_scores_value` int,
+    `reviews_per_month` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `listing_diagnostics` (
-    `listing_diagnostics_id` int(11),
-    `scrapings_id` int(11) FOREIGN KEY(scrapings_id) REFERENCES scrapings(scrapings_id),
-    `neighbourhood_id` int(11) FOREIGN KEY(neighbourhood_id) REFERENCES neighbourhoods(neighbourhood_id),
-    `minmax_insights_id` int(11) FOREIGN KEY(minmax_insights_id) REFERENCES minmax_insights(minmax_insights_id),
-    `avail_id` int(11) FOREIGN KEY(avail_id) REFERENCES availability_info(avail_id),
-    `hld_id` int(11) FOREIGN KEY(hld_id) REFERENCES hld(hld_id),
-    `hqad_id` int(11) FOREIGN KEY(hqad_id) REFERENCES hqad(hqad_id),
-    `reviews_diagnostics_id` int(11) FOREIGN KEY(reviews_diagnostics_id) REFERENCES reviews_diagnostics(reviews_diagnostics_id),
-    `property_id` int(11) FOREIGN KEY(property_id) REFERENCES property(property_id),
+CREATE TABLE IF NOT EXISTS `listings` (
+    `listing_id` int PRIMARY KEY,
+    `scrapings_id` int,
+    `host_id` int,
+    `neighbourhood_id` int,
+    `property_id` int,
+    `minmax_insights_id` int,
+    `avail_info_id` int,
+    `rev_diag_id` int,
     `listing_url` varchar(100),
     `name` varchar(100),
     `picture_url` varchar(100),
     `license` varchar(100),
-    `instant_bookable` tinyint(1)
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+    `instant_bookable` tinyint,
+    FOREIGN KEY(scrapings_id)
+        REFERENCES scrapings(scrapings_id),
+    FOREIGN KEY(host_id)
+        REFERENCES host(host_id),
+    FOREIGN KEY(neighbourhood_id)
+        REFERENCES neighbourhoods(neighbourhood_id),
+    FOREIGN KEY(property_id)
+        REFERENCES property(property_id),
+    FOREIGN KEY(minmax_insights_id)
+        REFERENCES minmax_insights(minmax_insights_id),
+    FOREIGN KEY(avail_info_id)
+        REFERENCES availability_info(avail_id),
+    FOREIGN KEY(rev_diag_id)
+        REFERENCES reviews_diagnostics(rev_diag_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
